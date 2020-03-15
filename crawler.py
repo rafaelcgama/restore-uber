8cf483533f4b22a1918fd0055f1439d4e03fecaa
Rafael <rafaelcgama@gmail.com>
2020-03-15 20:38:23 -0300

    Seep() additions

--- crawler.py
+++ crawler.py
@@ -20,8 +20,9 @@ class LinkedInCrawler():
     def __init__(self):
         self.driver = init_driver()
         self.url = 'https://www.linkedin.com/home'
+        self.wait = WebDriverWait(self.driver, 120)
         self.company = 'Uber'
-        self.cities = ['San Francisco', 'São Paulo']
+        self.cities = ['San Francisco']# , 'São Paulo']
         self.xpaths = {
             'locations': {
                 'filter': './/button[contains(@aria-label,"Locations filter")]',
@@ -45,13 +46,14 @@ class LinkedInCrawler():
         :param tries: int keeping track of the number of attempts to login
         '''
         try:
-            logger.info('Attempting to login to the website')
+            logger.info(f'Attempting to login to {self.url}')
             self.driver.find_element_by_xpath('.//input[@name="session_key"]').clear()
             self.driver.find_element_by_xpath('.//input[@name="session_key"]').send_keys(MY_USERNAME)
             self.driver.find_element_by_xpath('.//input[@name="session_password"]').clear()
             self.driver.find_element_by_xpath('.//input[@name="session_password"]').send_keys(PASSWORD)
             self.driver.find_element_by_xpath('.//button[@class="sign-in-form__submit-btn"]').click()
             sleep(2)
+            logger.info('')
 
         except Exception as e:
             logger.error('Login attempt failed', exc_info=True)
@@ -63,7 +65,7 @@ class LinkedInCrawler():
 
 
     def wait_for_element(self, xpath):
-        WebDriverWait(self.driver, 120).until(EC.presence_of_element_located((By.XPATH, xpath)))
+        self.wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
 
     def select_city(self, city):
         '''
@@ -81,12 +83,12 @@ class LinkedInCrawler():
 
         sleep(1)
         self.driver.find_element_by_xpath(self.xpaths['locations']['selection'].format(city)).click()
-
+        sleep(1)
         self.wait_for_element(self.xpaths['locations']['apply_button'])
         self.driver.find_element_by_xpath(self.xpaths['locations']['apply_button']).click()
         sleep(1)
 
-        logger.info('City selection: ENDED')
+        logger.info('City selection: FINISHED')
 
 
     def add_company(self, company):
@@ -107,86 +109,104 @@ class LinkedInCrawler():
         # self.wait_for_element(self.xpaths['company']['selection'])
         sleep(1)
         self.driver.find_element_by_xpath(self.xpaths['company']['selection'].format(company)).click()
-
+        sleep(1)
         self.wait_for_element(self.xpaths['company']['apply_button'])
         self.driver.find_element_by_xpath(self.xpaths['company']['apply_button']).click()
         sleep(1)
 
-        logger.info('Company selection: ENDED')
+        logger.info('Company selection: FINISHED')
 
         # WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.ID, 'pageBody')))
 
-    # def extract_page(self, combobox, employee_list):
-    #     '''
-    #     Extract employee info of one page
-    #     :return: A list
-    #     '''
-    #     for employee in combobox:
-    #         name = employee.find_element_by_xpath('.//span[@class="name actor-name"]').text
-    #         position = employee.find_element_by_xpath(
-    #             './/p[contains(@class,"t-black t-normal search-result__truncate")]').text
-    #         employee_list.append({
-    #             'name': name,
-    #             'position': position
-    #         })
-
-
-    def get_employees_info(self, tries=0):
+    def extract_page(self, employee_list , total, tries=0):
         '''
-        Iterate through employees and collect name and position
-        :return: A list() of dict() containing an employee name and position
+        Extract employee info of one page
+        :return: A list
         '''
-        logger.info('Started extracting employee name and position')
-        employee_info = []
-        next_button_xpath = './/button[@aria-label="Next"]'
-        # TODO ta travando aqui
-        self.wait_for_element(next_button_xpath)
-        employee_combox_xpath = './/ul[contains(@class,"search-results")]/li'
-
-        while self.driver.find_element_by_xpath(next_button_xpath) is not None:
-            logger.info(f'Extracting employee data from page {tries + 1}')
+        try:
+            logger.info(f'Employee data from page number {total}: STARTED')
+            next_button_xpath = './/button[@aria-label="Next"]'
+            employee_combox_xpath = './/ul[contains(@class,"search-results")]/li'
             employee_combox = self.driver.find_elements_by_xpath(employee_combox_xpath)
             for employee in employee_combox:
                 name = employee.find_element_by_xpath('.//span[@class="name actor-name"]').text
                 position = employee.find_element_by_xpath(
                     './/p[contains(@class,"t-black t-normal search-result__truncate")]').text
-                employee_info.append({
+                employee_list.append({
                     'name': name,
                     'position': position
                 })
-                self.driver.find_element_by_xpath(next_button_xpath).click()
-                self.driver.find_element_by_xpath(next_button_xpath)
-                tries += 1
+            self.driver.find_element_by_xpath(next_button_xpath).click()
+            self.wait_for_element(employee_combox_xpath)
+            self.driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+            self.wait_for_element(next_button_xpath)
+            logger.info(f'Employee data from page number {total}: COMPLETED')
 
+        except Exception as e:
+            logger.info(f'Failure to collect employee info from page {total}')
+            if tries < MAX_TRIES:
+                self.driver.refresh()
+                logger.info(f'Reattempting to collect employee data from page {total}')
+                return self.extract_page(employee_list, total)
+            else:
+                logger.error(f'Failed to load an extract info from page number {total}')
+
+
+    def get_employees_info(self):
+        '''
+        Iterate through employees and collect name and position
+        :return: A list() of dict() containing an employee name and position
+        '''
+        logger.info('Employee data extraction: STARTED')
+        self.driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+        employee_info = []
+        next_button_xpath = './/button[@aria-label="Next"]'
+        self.wait_for_element(next_button_xpath)
+
+        page_count = 1
+
+        # while self.driver.find_element_by_xpath(next_button_xpath) is not None:
+        #     self.extract_page(employee_info, total)
+        #     total += 1
+
+        logger.info('Employee data extraction: FINISHED')
         return employee_info
 
 
-    def get_data(self):
+    def get_data(self, tries=0):
+        try:
+            self.driver.get(self.url)
+            sleep(2)
 
-        self.driver.get(self.url)
-        # WebDriverWait(self.driver, 120).until(EC.presence_of_element_located((By.NAME, 'session_key')))
-        sleep(2)
+            self.login()
 
-        self.login()
+            data = []
+            for city in self.cities:
 
-        # Go to people search page
-        self.driver.find_element_by_xpath('.//div[@id="global-nav-typeahead"]').click()
-        sleep(1)
-        self.driver.find_element_by_xpath('.//li[@aria-label="Search for people"]').click()
-        sleep(1)
+                # Go to people search page
+                self.driver.find_element_by_xpath('.//div[@id="global-nav-typeahead"]').click()
+                sleep(1)
+                self.driver.find_element_by_xpath('.//li[@aria-label="Search for people"]').click()
+                sleep(1)
 
-        data = []
-        for city in self.cities:
-            self.select_city(city)
+                self.select_city(city)
 
-            self.add_company(self.company)
+                self.add_company(self.company)
 
-            employees_info = self.get_employees_info()
-            data.append(employees_info)
+                employee_info_in_city = self.get_employees_info()
+                data.append(employee_info_in_city)
 
-        return data
+            return data
 
+        except Exception as e:
+            logger.info(f'Error collecting employee data from all cities')
+            if tries < MAX_TRIES:
+                self.driver.refresh()
+                logger.info(f'Reattempting data collection...')
+                return self.get_data()
+            else:
+                logger.error('Failure to complete data collection')
 
 if __name__ == '__main__':
     crawler = LinkedInCrawler()
-    crawler.get_data()
+    data = crawler.get_data()
--- utils.py
+++ utils.py
@@ -4,10 +4,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 def init_driver():
     chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--start-maximized")
     # chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
     driver = webdriver.Chrome(chrome_options=chrome_options)
+    # driver.implicitly_wait(1)
     driver_ready(driver)
 
     return driver
